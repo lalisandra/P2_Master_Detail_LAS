719b9a10c0f969321f7f972495f24d1485ad50a6
lalisandra <l.alisandrasenabre@edu.gva.es>
2023-10-28 17:16:05 +0200

    Front End

---
+++ app/src/main/java/com/example/p2_master_detail_las/model/Show.kt
@@ -0,0 +1,49 @@
+package com.example.p2_master_detail_las.model
+
+import android.content.Context
+import com.google.firebase.crashlytics.buildtools.reloc.com.google.common.reflect.TypeToken
+import com.google.gson.Gson
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.lang.reflect.Type
+import com.example.p2_master_detail_las.R
+
+data class Show(
+    var id:Int,
+    var name:String,
+    var language:String,
+    var genres:Array<String>,
+    var status:String,
+    var premiered:String,
+    var officialSite:String,
+    var rating:Float,
+    var image:String,
+    var summary:String
+){
+    companion object{
+        val showList:MutableList<Show> = mutableListOf()
+
+        fun loadSeries(context: Context){
+            val raw = context.resources.openRawResource(R.raw.datos_json)
+            val rd = BufferedReader(InputStreamReader(raw))
+
+            val listType: Type = object : TypeToken<MutableList<Show>?>() {}.type
+
+            val gson = Gson()
+            showList.clear()
+
+            val shows:List<Show> = gson.fromJson(rd, listType)
+
+            showList.addAll(shows)
+        }
+
+        fun getShowById(id:Int): Show?{
+            val series = showList.filter {
+                it.id == id
+            }
+
+
+            return if(series.isNotEmpty()) series[0] else null
+        }
+    }
+}
